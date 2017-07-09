878fc71d4140d9f3aa482b1330841d7fe9d946cd
Malcolm <malichaooo@gmail.com>
2017-07-09 00:35:38 -0700

    Update the docker file for MPC quizzes project

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
-docker build\
-  --build-arg user=$USER\
-  --build-arg uid=$UID\
+
+# Check args
+if [ "$#" -ne 3 ]; then
+  echo "usage: ./build.sh user_name user_id group_id"
+  exit
+fi
+
+echo USER = $1
+echo UID = $2
+echo GID = $3 		#cut -d: -f3 < <(getent group sudo)
+echo HOME = $HOME
+echo SHELL = $SHELL
+
+sudo docker build\
+  --build-arg user=$1\
+  --build-arg uid=$2\
+  --build-arg gid=$3\
   --build-arg home=$HOME\
   --build-arg shell=$SHELL\
   -t carnd-term2 .
