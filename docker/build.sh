22d9c07df88d03ed4cec9ed3fa78b2d8f82b194a
Malcolm <malichaooo@gmail.com>
2017-07-09 14:53:22 -0700

    Add qt creator for coding

--- docker/build.sh
+++ docker/build.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-# Check args
-if [ "$#" -ne 3 ]; then
-  echo "usage: ./build.sh user_name user_id group_id"
-  exit
-fi
-
-echo USER = $1
-echo UID = $2
-echo GID = $3 		#cut -d: -f3 < <(getent group sudo)
-echo HOME = $HOME
-echo SHELL = $SHELL
-
+group_id=$(cut -d: -f3 < <(getent group sudo))
+echo $group_id
 sudo docker build\
-  --build-arg user=$1\
-  --build-arg uid=$2\
-  --build-arg gid=$3\
+  --build-arg user=$USER\
+  --build-arg uid=$UID\
+  --build-arg gid=$group_id\
   --build-arg home=$HOME\
   --build-arg shell=$SHELL\
   -t carnd-term2 .
